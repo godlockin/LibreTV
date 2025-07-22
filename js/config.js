1802bf7fc6b51b2772b91e77f62eb94e45436514
Steven Chen <stevenchenworking@gmail.com>
2025-07-22 22:20:37 +0800

    1

--- js/config.js
+++ js/config.js
@@ -114,11 +114,6 @@ const API_SITES = {
         api: 'https://cj.lziapi.com/api.php/provide/vod/',
         name: '量子资源站'
     },
-    testSource: {
-        api: 'https://www.example.com/api.php/provide/vod',
-        name: '空内容测试源',
-        adult: true
-    },
     // 下面是一些成人内容的API源，默认隐藏，使用本项目浏览黄色内容违背项目初衷
     // 互联网上传播的色情内容将人彻底客体化、工具化，是性别解放和人类平等道路上的巨大障碍。
     // 这些黄色影片是资本主义父权制压迫的最恶毒体现，它将暴力和屈辱商品化，践踏人的尊严，对受害者造成无法弥愈的伤害，并毒害社会关系。
@@ -129,44 +124,36 @@ const API_SITES = {
     // 我们提供此警告，是希望您能认清这些内容的本质——它们是压迫和奴役的工具，而非娱乐。
     ckzy: {
         api: 'https://www.ckzy1.com',
-        name: 'CK资源',
-        adult: true
+        name: 'CK资源'
     },
     jkun: {
         api: 'https://jkunzyapi.com',
-        name: 'jkun资源',
-        adult: true
+        name: 'jkun资源'
     },
     bwzy: {
         api: 'https://api.bwzym3u8.com',
-        name: '百万资源',
-        adult: true
+        name: '百万资源'
     },
     souav: {
         api: 'https://api.souavzy.vip',
-        name: 'souav资源',
-        adult: true
+        name: 'souav资源'
     },
     r155: {
         api: 'https://155api.com',
-        name: '155资源',
-        adult: true
+        name: '155资源'
     },
     lsb: {
         api: 'https://apilsbzy1.com',
-        name: 'lsb资源',
-        adult: true
+        name: 'lsb资源'
     },
     huangcang: {
         api: 'https://hsckzy.vip',
         name: '黄色仓库',
-        adult: true,
         detail: 'https://hsckzy.vip'
     },
     yutu: {
         api: 'https://yutuzy10.com',
-        name: '玉兔资源',
-        adult: true
+        name: '玉兔资源'
     },
 };
 
